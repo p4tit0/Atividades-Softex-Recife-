e99e3b04114f106cb236a158bccb0c1ceea80665
Vinícius Santos <66649091+p4tit0@users.noreply.github.com>
2022-06-25 17:59:43 -0300

    Update main.py

--- Lógica de Programação e Orientação a Objetos/Introdução a programação/Atividade 05/main.py
+++ Lógica de Programação e Orientação a Objetos/Introdução a programação/Atividade 05/main.py
@@ -21,8 +21,6 @@ def calculadora(n1, n2, op):
         return n1 * n2
     elif op == 4:
         return n1 / n2
-    else:
-        return 0
 
 
 format.title(50, "CALCULADORA 2.0", bg_color='BLACK', txt_color='YELLOW', txt_format='BOLD+ITALIC+INVERT')
@@ -36,6 +34,9 @@ while True:
     op = int_input("Operação escolhida: ", "INSIRA UM NÚMERO")
     if op == 0:
         break
+    if op not in range(1, 5):
+        format.msg1(50, "ERROR", "ESSA OPÇÃO NÃO EXISTE")
+        continue
     n1 = int_input("Primeiro número: ", "INSIRA UM NÚMERO")
     n2 = int_input("Segundo número: ", "INSIRA UM NÚMERO")
     format.msg1(50, "RESULTADO", f"{calculadora(n1, n2, op)}", bg_color='', txt_format='BOLD')
